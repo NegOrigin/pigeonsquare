315dcdab684905b6585de9c2ec7615a963c542f5
NegOrigin <corentin.frery@gmail.com>
2017-10-15 19:05:54 -0400

    v0.11
    Suppression de code commenté

--- src/modele/Pigeon.java
+++ src/modele/Pigeon.java
@@ -113,57 +113,6 @@ public class Pigeon {
             }
         });
         new Thread(sleeper).start();
-		
-		/*//Si pigeon non en fuite
-		if(!enFuite) {
-			//Réinitialisation recherche
-			graineAppetissante = null;
-			distanceMin = 5000;
-			
-			//Repérer la graine la plus appetissante
-			for (int i = 0; i < graines.size(); i++) {
-				Graine graine = graines.get(i);
-				
-				//Test graine bonne
-				if(graine.getStatut() == 0) {
-					Circle graineCircle = graine.getCircle();
-					double distance;
-					
-					//Calcul distance
-					distance = Math.sqrt(
-							Math.pow(
-								Math.abs(graineCircle.getTranslateX()
-								- circle.getTranslateX()), 2)
-							+ Math.pow(
-								Math.abs(graineCircle.getTranslateY()
-								- circle.getTranslateY()), 2));
-					
-					//Test distance plus courte
-					if(distance < distanceMin) {
-						//Mise à jour graine cible
-						graineAppetissante = graine;
-						distanceMin = distance;
-					}
-				}
-			}
-			
-			//Aller vers la graine appetissante
-			transition.stop();
-			if(graineAppetissante != null) {
-				transition.setToX(graineAppetissante.getCircle().getTranslateX());
-				transition.setToY(graineAppetissante.getCircle().getTranslateY());
-				transition.setDuration(Duration.seconds(distanceMin*0.01));
-		        transition.setNode(circle);
-		        transition.play();
-		        
-		        //Manger graine
-		        transition.setOnFinished(new EventHandler<ActionEvent>() {
-		        	public void handle(ActionEvent event) {
-		        		graineAppetissante.etreMange();
-					}
-		        });
-			}
-		}*/
 	}
 	
 	public void fuir() {
